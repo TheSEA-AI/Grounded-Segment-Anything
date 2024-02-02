e30c40a596c920f456f377f04b766341c10e546a
LinchuanXUTheSEAAI <unilin54092@gmail.com>
2024-02-02 16:13:06 +0900

    image outline extraction

--- image_outline_extraction.py
+++ image_outline_extraction.py
@@ -230,18 +230,18 @@ def product_outline_extraction(intput_dir, output_dir, product_type = "cosmetic 
         mask_all = np.stack((mask_all,)*3, axis=-1)
         ################
 
-        mask_all = mask_all * np.uint8(255) 
+        #mask_all = mask_all * np.uint8(255) 
 
         img = Image.open(img_path).convert("RGB")
         image_array = np.asarray(img)
-        image_array = np.where(image_array == 0, 255, image_array)
+        #image_array = np.where(image_array == 0, 255, image_array)
         #print(f'image_array before = {image_array}')
-        image_array = image_array * mask_all 
-        image_array = np.where(image_array == 0, image_array, 255)
+        #image_array = image_array * mask_all 
+        #image_array = np.where(image_array == 0, image_array, 255)
         #print(f'image_array after = {image_array}')
         
         hed = HWC3(image_array)
-        hed = hedDetector(hed)
+        hed = hedDetector(hed) * mask_all[:,:,0]
         hed = HWC3(hed)
         hed = cv2.resize(hed, (image_resolution, image_resolution),interpolation=cv2.INTER_LINEAR)
         img_masked = Image.fromarray(hed)
